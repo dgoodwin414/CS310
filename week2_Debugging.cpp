b9791b17d551ebfdf6d40158848d492be3ebc3d7
Dalton Goodwin <djgoodwin@my365.bellevue.edu>
2026-03-25 22:01:53 -0500

    Add files via upload

---
+++ week2_Debugging.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string favoriteGame;
+    int hoursPlayed;
+
+    cout << "What is your favorite video game? ";
+    cin >> favoriteGame;
+
+    cout << "How many hours have you played it? ";
+    cin >> hoursPlayed
+
+    cout >> "Your favorite game is " << favoriteGame 
+         << " and you have played it for " << hoursPlayed 
+         << " hours." << endl;
+
+    return 0;
+}
+
+
+
